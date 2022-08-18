f3d040504b3f7ea35485c0449e2a6d5b2af8f31b
Alexander Piontek <nekom113@mail.ru>
2022-08-18 15:39:32 +0300

    18.08 15.39 start

---
+++ js/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsContent = document.querySelectorAll('.tabcontent');
+  const tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach((el) => {
+      el.classList.add('hide');
+      el.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (event) => {
+    const { target } = event;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target === item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
