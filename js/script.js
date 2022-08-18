f810766bfa631a6feac1bc7494e2efc9a4bffc5c
Alexander Piontek <nekom113@mail.ru>
2022-08-18 17:46:39 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -34,4 +34,51 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+  // Timer
+  const deadline = '2022-08-19'
+
+
+  function getTimeRemaining(endtime){
+    const timeDifference = Date.parse(endtime) - Date.parse(new Date());
+    const daysCount = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hoursCount = Math.floor(timeDifference / (1000 * 60 * 60) % 24);
+    const minutesCount = Math.floor((timeDifference / 1000 / 60) % 60);
+    const secondsCount = Math.floor((timeDifference / 1000) % 60);
+
+    return {timeDifference, daysCount, hoursCount, minutesCount, secondsCount}
+  }
+
+  function getNumbersWhithZero(number){
+    if(number >=0 && number < 10){
+      return `0${number}`
+    } else{
+      return number
+    }
+  }
+
+  function setClock(selector, endtime){
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+
+    const timeInterval = setInterval(updateClock, 1000)
+
+    updateClock();
+
+    function updateClock(){
+      const {timeDifference, daysCount, hoursCount, minutesCount, secondsCount} = getTimeRemaining(endtime)
+      days.innerHTML = getNumbersWhithZero(daysCount);
+      hours.innerHTML = getNumbersWhithZero(hoursCount);
+      minutes.innerHTML = getNumbersWhithZero(minutesCount);
+      seconds.innerHTML = getNumbersWhithZero(secondsCount);
+
+      if(timeDifference <= 0){
+        clearInterval(timeInterval);
+      } 
+    }
+  }
+
+  setClock('.timer', deadline)
 });
